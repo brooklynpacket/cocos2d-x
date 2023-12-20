5c6971622dc07874f5bbe19d99b005b72c1d28fe
Ian Clarke <ian.clarke@jamcity.com>
2023-12-20 14:55:21 -0800

    iclarke_init: Fixed lua link errors

--- cocos/scripting/lua-bindings/external/lua/cjson/lua_cjson.c
+++ cocos/scripting/lua-bindings/external/lua/cjson/lua_cjson.c
@@ -1325,7 +1325,7 @@ static int json_decode(lua_State *l)
  *
  * luaL_setfuncs() is used to create a module table where the functions have
  * json_config_t as their first upvalue. Code borrowed from Lua 5.2 source. */
-static void luaL_setfuncs (lua_State *l, const luaL_Reg *reg, int nup)
+void luaL_setfuncs (lua_State *l, const luaL_Reg *reg, int nup)
 {
     int i;
 
